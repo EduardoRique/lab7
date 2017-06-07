82b28624ae5045eec7ab2f6d5cde6b7cbcca871a
EduardoRique <luiseduardorique@gmail.com>
2017-06-07 11:19:58 -0300

    11:19 - 07/06/2017
    Tarefa2 incompleta

--- tarefa2.cpp
+++ tarefa2.cpp
@@ -3,6 +3,17 @@
 
 using namespace std;
 
+template<typename TContainer>
+void print_elementos(const TContainer& collection, const char* label="", const char separator=' '){
+    cout << label;
+    
+    set<int>::iterator it;
+    for (it = collection.begin(); it != collection.end(); ++it){
+        cout << *it;
+        cout << separator;
+    }
+}
+
 int main() {
 set<int> numeros;
 
@@ -13,4 +24,7 @@ numeros.insert(1);
 numeros.insert(2);
 numeros.insert(5);
 
+print_elementos(numeros, "CSV Set: ", ';');
+
     return 0;
+}
